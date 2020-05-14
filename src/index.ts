7a0554d67685b1097af7c8591e1168b2ea5ca9f7
Guilherme Gomes <gtechmedia@gmail.com>
2020-05-13 22:59:47 -0300

    Repo refactor

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-require('dotenv').config()
-const { App } = require('./controllers/AppController')
-
-// Criando instância da aplicação
-const app = new App()
--- src/models/Codenation.ts
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-export interface Codenation {
-  numero_casas: number,
-  token: string,
-  cifrado: string,
-  decifrado: string,
-  resumo_criptografico: string
-}
